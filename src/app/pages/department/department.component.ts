67b6708549e5046444b78ab48767fb31d50ce9b0
Abner A Oliveira <abneralves10@hotmail.com>
2020-04-13 20:09:55 -0300

    Created service for consume department api

--- src/app/pages/department/department.component.ts
+++ src/app/pages/department/department.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Department} from "./department";
+import {DepartmentService} from "./department.service";
 
 @Component({
   selector: 'app-department',
@@ -7,9 +9,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DepartmentComponent implements OnInit {
 
-  constructor() { }
+  depName: string = '';
+  departments: Department[] = [
+    {name: 'Dep 1', _id: 'asfdadsf'},
+    {name: 'Dep 2', _id: 'asfdadsf'},
+    {name: 'Dep 3', _id: 'asfdadsf'},
+  ];
+
+  constructor(private departmentService: DepartmentService) {
+  }
 
   ngOnInit() {
+    this.departmentService.getDepartments().subscribe((deps) => {
+      this.departments = deps;
+    })
+  }
+
+  save() {
+    this.departmentService.addDepartment({name: this.depName}).subscribe(
+      (dep) => {
+        console.log(dep);
+      },
+      (error) => console.log(error)
+    )
+  }
+
+  clearFields() {
+    this.depName = '';
+  }
+
+  cancel() {
+
+  }
+
+  edit(dep: Department) {
+
+  }
+
+  delete(dep: Department) {
+
   }
 
 }
--- src/app/pages/department/department.service.ts
+++ src/app/pages/department/department.service.ts
@@ -1,9 +1,32 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {BehaviorSubject, Observable} from "rxjs";
+import {Department} from "./department";
+import {tap} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DepartmentService {
 
-  constructor() { }
+  readonly url = "http://localhost:3000/departments";
+  private departmentSubject: BehaviorSubject<Department[]> = new BehaviorSubject<Department[]>(null);
+  private loaded: boolean = false;
+
+  constructor(private http: HttpClient) {
+  }
+
+  getDepartments(): Observable<Department[]> {
+    if (!this.loaded) {
+      this.http.get<Department[]>(this.url)
+        .pipe(tap(deps => console.log(deps)))
+        .subscribe(this.departmentSubject);
+    }
+    return this.departmentSubject.asObservable();
+  }
+
+  addDepartment(d: Department): Observable<Department> {
+    return this.http.post<Department>(this.url, d)
+      .pipe(tap((dep: Department) => this.departmentSubject.getValue().push(dep)));
+  }
 }
